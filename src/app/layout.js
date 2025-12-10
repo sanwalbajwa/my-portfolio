72caae56e57142166a289ef3ad5fdbb2e405eb84
Sanwal Bajwa <166105367+sanwalbajwa@users.noreply.github.com>
2025-12-10 16:22:45 +0500

    Updating website url in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ const poppins = Poppins({
 })
 
 export const metadata = {
-  metadataBase: new URL('https://sanwalbajwa.com/'), // Replace with your actual domain
+  metadataBase: new URL('https://www.sanwalbajwa.com/'),
   
   title: {
     default: 'Sanwal Bajwa | Full Stack Developer',
