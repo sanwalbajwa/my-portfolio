aa111842fa311733722769106ea243efce0473e1
Sanwal Bajwa <166105367+sanwalbajwa@users.noreply.github.com>
2026-01-09 16:29:15 +0500

    added the X-Robots-Tag

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,19 @@ const nextConfig = {
       }
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
